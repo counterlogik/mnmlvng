3b51b8c565631e71bb07483e27c0185804fd0622
Addison Staples <jastaplesiv@gmail.com>
2016-11-14 22:04:27 -0600

    refine Item Card compent and sub components; layouts groundwork and structure css files and organization

--- src/App.js
+++ src/App.js
@@ -1,33 +1,75 @@
 import React, { Component } from 'react';
 import './App.css';
 import './ionicons.css';
+import shirtPicture from './img/uo_stevens_shirt.jpg';
+//import shirtData from './mnmlvng.json';
+
+const shirtData = {
+  'Description': 'Shirt (medium)',
+  'Category': 'Wardrobe',
+  'Model': 'UO Stevens Cross-Dyed Button-Down Shirt',
+  'Link': 'http://www.urbanoutfitters.com/urban/catalog/productdetail.jsp?id=39390299&category=M_TOPS_SHIRTS&color=041',
+  'Count': 1,
+  'Spark': 'Love',
+  'Location': 'living room; closet',
+  'Notes': 'first medium I purchased after losing weight in 2016'
+}
+
+const categoryToIconCharacterMap = {
+  'Wardrobe': 'tshirt-outline'
+};
 
 class ItemDetail extends Component {
   render() {
-    return <div className="itemDetail">{this.props.itemDescription}</div>;
+    console.log(categoryToIconCharacterMap[this.props.category]);
+    let icon = this.props.icon || categoryToIconCharacterMap[this.props.category] || '';
+    let addClasses = this.props.addClasses || '';
+    return <div className="ItemDetail">
+      {icon && <PrefixIcon icon={icon} />}
+        <div className={'entryText' + addClasses}>
+          {this.props.description}
+        </div>
+    </div>;
   }
 }
 
 class ItemImage extends Component {
   render() {
-    return <div className="itemImage"><img src={this.props.itemImage} alt={this.props.category}></img></div>;
+    var divStyle = {
+      backgroundImage: 'url(' + this.props.image + ')'
+    };
+    return <div className="ItemImage" style={divStyle} alt={this.props.category}></div>;
   }
 }
 
-const LinkIcon = () => (
-    <div className="LinkIcon"> </div>
-);
+
+
+class PrefixIcon extends Component {
+    render() {
+      return <div className="PrefixIcon"><i className={'icon ion-'+this.props.icon} /> </div>;
+    }
+}
+
+class ItemCard extends Component {
+  render() {
+    return <div className="ItemCard">
+      <ItemImage image={shirtPicture} category={this.props.details.Category} />
+      <div className="itemContent">
+        <ItemDetail category={this.props.details.Category} description={this.props.details.Description} />
+        <ItemDetail icon={'link'} description={this.props.details.Model} />
+        <ItemDetail icon={'android-locate'} description={this.props.details.Location} />
+        <ItemDetail description={this.props.details.Notes} />
+      </div>
+    </div>;
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="itemCard">
-          <ItemImage itemImage={this.props.image} />
-          <ItemDetail prefixIcon={this.props.category} itemDescription={`Button-down shirt (medium)`} />
-          <ItemDetail prefixIcon={LinkIcon} itemDescription={`Urban Outfitters Stevens Cross-Dyed Button-Down`} />
-          <ItemDetail prefixIcon="" itemDescription={`living room; closet`} />
-          <ItemDetail itemDescription={`This is the first medium size shirt I purchased after losing over 55 lbs in 2016.`} />
+        <div className="container">
+          <ItemCard details={shirtData} />
         </div>
       </div>
     );
