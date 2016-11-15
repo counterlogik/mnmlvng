f33871210a477572da55325750de233909a6d623
Addison Staples <jastaplesiv@gmail.com>
2016-11-15 01:06:40 -0600

    more closely follow material design spec... may deviate at design polish

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import shirtPicture from './img/uo_stevens_shirt.jpg';
 const shirtData = {
   'Description': 'Shirt (medium)',
   'Category': 'Wardrobe',
-  'Model': 'UO Stevens Cross-Dyed Button-Down Shirt',
+  'Model': 'UO Stevens Cross-Dyed',
   'Link': 'http://www.urbanoutfitters.com/urban/catalog/productdetail.jsp?id=39390299&category=M_TOPS_SHIRTS&color=041',
   'Count': 1,
   'Spark': 'Love',
@@ -23,12 +23,12 @@ class ItemDetail extends Component {
   render() {
     console.log(categoryToIconCharacterMap[this.props.category]);
     let icon = this.props.icon || categoryToIconCharacterMap[this.props.category] || '';
-    let addClasses = this.props.addClasses || '';
-    return <div className="ItemDetail">
+    let addClasses = this.props.addClasses ? ' ' + this.props.addClasses : '';
+    return <div className="ItemDetail" href={this.props.link}>
       {icon && <PrefixIcon icon={icon} />}
-        <div className={'entryText' + addClasses}>
-          {this.props.description}
-        </div>
+      <div className={'entryText' + addClasses} title={this.props.url}>
+        {this.props.description}
+      </div>
     </div>;
   }
 }
@@ -55,10 +55,10 @@ class ItemCard extends Component {
     return <div className="ItemCard">
       <ItemImage image={shirtPicture} category={this.props.details.Category} />
       <div className="itemContent">
-        <ItemDetail category={this.props.details.Category} description={this.props.details.Description} />
-        <ItemDetail icon={'link'} description={this.props.details.Model} />
+        <ItemDetail category={this.props.details.Category} description={this.props.details.Description} addClasses="title" />
+        <ItemDetail icon={'link'} description={this.props.details.Model} url={this.props.details.Link} />
         <ItemDetail icon={'android-locate'} description={this.props.details.Location} />
-        <ItemDetail description={this.props.details.Notes} />
+        <ItemDetail description={this.props.details.Notes} addClasses="notes" />
       </div>
     </div>;
   }
